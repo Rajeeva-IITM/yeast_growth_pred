2b1e79e68f0279ece0d351de93e9d851d4ff25be
Rajeeva-IITM <bt20d204@smail.iitm.ac.in>
2024-01-02 15:18:07 +0530

    Changes to be committed: 	modified:   __pycache__/network.cpython-311.pyc 	modified:   eval_model.ipynb 	modified:   lightning_model.py 	modified:   network.py

--- lightning_model.py
+++ lightning_model.py
@@ -4,7 +4,7 @@ import torch
 # from lightning.pytorch.callbacks import Callback
 import torchmetrics
 from pytorch_lightning import LightningModule, Trainer
-from network import Net
+from network import Net, MultiViewNet
 from torch import nn
 
 from data import EncodeModule
@@ -218,30 +218,253 @@ class Netlightning(LightningModule):
         
         return None
     
+class NetMultiViewLightning(LightningModule):
+    
+    def __init__(
+        self,
+        layers_before_concat: List[int],
+        layers_after_concat: List[int],
+        input_size: List[int],
+        output_size: int,
+        dropout: float,
+        activation: str,
+        lr: float,
+        max_lr: float,
+        weight_decay: float,
+        task: str = "regression",
+        loss_function: str = "mse",
+    ):
+        """_summary_
+
+        Args:
+            layers_before_concat (List[int]): _description_
+            layers_after_concat (List[int]): _description_
+            input_size (List[int]): _description_
+            output_size (int): _description_
+            dropout (float): _description_
+            activation (str): _description_
+            lr (float): _description_
+            max_lr (float): _description_
+            weight_decay (float): _description_
+            task (str, optional): Task of the model. Defaults to "regression".
+            loss_function (str, optional): _description_. Defaults to "mse".
+        """
+        
+        super(NetMultiViewLightning, self).__init__()
+        
+        if task == "regression":
+            # Map loss function names to their respective loss functions
+            loss_functions = {
+                "mse": nn.MSELoss(),
+                "mae": nn.L1Loss(),
+                "huber": nn.HuberLoss()
+            }
+            
+            metrics = {
+                "mae": torchmetrics.MeanAbsoluteError(),
+                "R2": torchmetrics.R2Score()
+            }
+            
+            # Check if the loss function is valid
+            if loss_function not in loss_functions:
+                raise ValueError("Loss function must be one of ['mse', 'mae', 'huber']")
+            
+            # Set the criterion based on the loss function
+            self.criterion = loss_functions[loss_function]
+        
+        elif task == "binary_classification":
+            
+            loss_functions = {
+                "bce": nn.BCEWithLogitsLoss(),
+                "cross_entropy": nn.CrossEntropyLoss()
+            }
+            
+            metrics = {
+                "accuracy": torchmetrics.Accuracy(task='binary'),
+                "auc-roc": torchmetrics.AUROC(task='binary'),
+                "f1": torchmetrics.F1Score(task='binary')
+            }
+            
+            if loss_function not in loss_functions:
+                raise ValueError("Loss function must be one of ['bce', 'cross_entropy']")
+            
+            self.criterion = loss_functions[loss_function]
+        
+        else:
+            raise ValueError("Task must be one of ['regression', 'binary_classification']")
+        
+        self.save_hyperparameters()
+        
+        
+        #Defining model
+        self.net = MultiViewNet(
+            layers_before_concat=layers_before_concat,
+            layers_after_concat=layers_after_concat,
+            input_size=input_size,
+            output_size=output_size,
+            dropout=dropout,
+            activation=activation,
+        )
+        
+        # Defining Metrics
+        
+        self.metric_names = []
+        for metric in metrics:
+            for prefix in ['train', 'val', 'test']:
+                self.metric_names.append(f"{prefix}_{metric}")
+                setattr(self, f"{prefix}_{metric}", metrics[metric])
+        
+        # Learning Parameters
+        self.lr = lr
+        self.weight_decay = weight_decay
+        self.max_lr = max_lr
+        
+    def forward(self, x):
+        return self.net(x)
+        
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(
+            self.parameters(),
+            lr=self.lr,
+            weight_decay=self.weight_decay,
+        )
+        scheduler = torch.optim.lr_scheduler.OneCycleLR(
+            optimizer,
+            max_lr=self.max_lr,
+            total_steps=self.trainer.estimated_stepping_batches,  
+        )
+        
+        return [optimizer], [scheduler]
+        
+    def training_step(self, batch, batch_idx):
+        X, y = batch
+        y_pred = self.forward(X)
+        loss = self.criterion(y_pred, y)
+        
+        self.log(
+            "train_loss",
+            loss,
+            on_step=True,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True
+        )
+        
+        for metric_name in filter(lambda x:'train'  in x, self.metric_names):
+            
+            getattr(self, metric_name).update(y_pred, y)
+            
+            self.log(
+                metric_name,
+                getattr(self, metric_name),
+                on_step=False,
+                on_epoch=True,
+                prog_bar=True,
+                logger=True
+            )
+        
+        
+        return loss
+        
+    def validation_step(self, batch, batch_idx):
+        X, y = batch
+        y_pred = self.forward(X)
+        loss = self.criterion(y_pred, y)
+        
+        self.log(
+            "val_loss",
+            loss,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True
+        )
+        
+        for metric_name in filter(lambda x:'val'  in x, self.metric_names):
+            getattr(self, metric_name).update(y_pred, y)
+            
+            self.log(
+                metric_name,
+                getattr(self, metric_name),
+                on_step=False,
+                on_epoch=True,
+                prog_bar=True,
+                logger=True
+            )
+        
+        
+            
+    def test_step(self, batch, batch_idx):
+        X, y = batch
+        y_pred = self.forward(X)
+        loss = self.criterion(y_pred, y)
+        
+        self.log(
+            "test_loss",
+            loss,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True
+        )
+        
+        for metric_name in filter(lambda x:'test' in x, self.metric_names):
+            getattr(self, metric_name).update(y_pred, y)
+            
+            self.log(
+                metric_name,
+                getattr(self, metric_name),
+                on_step=False,
+                on_epoch=True,
+                prog_bar=True,
+                logger=True
+            )
+        
+            
 if __name__ == "__main__":
     
     torch.set_float32_matmul_precision("high")
     
-    model = Netlightning(
-        input_size=6078,
-        output_size=1,
-        hidden_layers=[100, 100],
+    # model = Netlightning(
+    #     input_size=6078,
+    #     output_size=1,
+    #     hidden_layers=[100, 100],
+    #     dropout=0.5,
+    #     lr=1e-3,
+    #     weight_decay=1e-5,
+    #     max_lr=1e-2,
+    #     activation="relu",
+    #     loss_function="mse",
+    # )
+    
+    # Small test
+    
+    model = NetMultiViewLightning(
+        layers_before_concat=[100, 100],
+        layers_after_concat=[100, 100],
+        input_size=[6014, 64],
+        output_size = 1,
         dropout=0.5,
-        lr=1e-3,
-        weight_decay=1e-5,
-        max_lr=1e-2,
         activation="relu",
-        loss_function="mse",
+        lr=1e-3,
+        max_lr=1e-2,
+        weight_decay=1e-5,
+        task="binary_classification",
+        loss_function="bce"
     )
     
+    # datamodule = EncodeModule(
+    #     path = "/storage/bt20d204/data/regression_data/bloom2013_regression.feather"
+    # )
+    
     datamodule = EncodeModule(
-        path="../data/regression_data/bloom2013_regression.feather"
+        path="/storage/bt20d204/data/bloom2013_clf_3_pubchem.feather"
     )
     
     trainer = Trainer(
         accelerator="gpu",
         devices=1,
-        fast_dev_run=10
+        fast_dev_run=100
     )
     
     trainer.fit(model, datamodule)
--- network.py
+++ network.py
@@ -95,18 +95,169 @@ class Net(nn.Module):
         """
         return self.sequence(x)
     
-    
+
+class MultiViewNet(nn.Module):
+    """Neural Network but with two views
+
+    Args:
+        nn (_type_): _description_
+    """
+
+    def __init__(
+        self,
+        layers_before_concat: List[int],
+        layers_after_concat: List[int],
+        input_size: List[int],
+        output_size: int,
+        dropout: float,
+        activation: str = "relu"
+    ):
+        """_summary_
+
+        Args:
+            layers_before_concat (List[int]): Number of layers before the concatenation
+            layers_after_concat (List[int]): Number of layers after the concatenation
+            input_size (List[int]): The input sizes of two views
+            output_size (int): The output size
+            dropout (float): Dropout
+            activation (str, optional): Activation function. Must be one of ["relu", "tanh", "sigmoid", "celu", "gelu"]. Defaults to "relu".
+        """
+        
+        if (len(layers_before_concat) < 1) or (len(layers_after_concat) < 1):
+            raise ValueError("Number of layers must be greater than 1.")
+
+        if dropout < 0 or dropout > 1:
+            raise ValueError("Dropout must be between 0 and 1.")
+        
+        if output_size < 1:
+            raise ValueError("Output size must be greater than 1.")
+        
+        if len(input_size) != 2:
+            raise ValueError("Input size must be 2.")
+        
+        if len(layers_after_concat) != len(layers_before_concat):  #TODO: This is only for simplicity's sake
+            raise ValueError("Number of layers before and after concatenation must be the same.")
+            
+        
+        super(MultiViewNet, self).__init__()
+        
+        self.layers_before_concat = layers_before_concat
+        self.layers_after_concat = layers_after_concat
+        self.input_size = input_size
+        self.output_size = output_size
+        self.dropout = dropout
+        
+        match activation:
+            case "relu":
+                self.activation = nn.ReLU()
+            case "tanh":
+                self.activation = nn.Tanh()
+            case "sigmoid":
+                self.activation = nn.Sigmoid()
+            case "celu":
+                self.activation = nn.CELU()
+            case "gelu":
+                self.activation = nn.GELU()
+            case _:
+                raise ValueError("Activation function not supported.")
+        
+        # Building the network
+        
+        self.view1 = [
+            nn.Linear(input_size[0], layers_before_concat[0], dtype=torch.float),
+            nn.BatchNorm1d(layers_before_concat[0], dtype=torch.float),
+            self.activation
+        ]
+        self.view2 = [
+            nn.Linear(input_size[1], layers_before_concat[0], dtype=torch.float),
+            nn.BatchNorm1d(layers_before_concat[0], dtype=torch.float),
+            self.activation
+        ]
+        
+        # postconcatenation
+        self.postconcat = [
+            nn.Linear(layers_before_concat[-1]*2,
+                      layers_after_concat[0],
+                      dtype=torch.float),
+            nn.BatchNorm1d(layers_after_concat[0], dtype=torch.float),
+            self.activation
+            ]
+        
+        for i in range(len(self.layers_before_concat)-2):
+            
+            self.view1.append(
+                nn.Linear(self.layers_before_concat[i], self.layers_before_concat[i+1], dtype=torch.float)
+            )
+            self.view2.append(
+                nn.Linear(self.layers_before_concat[i], self.layers_before_concat[i+1], dtype=torch.float)
+            )
+            self.view1.append(nn.BatchNorm1d(self.layers_before_concat[i+1], dtype=torch.float))
+            self.view2.append(nn.BatchNorm1d(self.layers_before_concat[i+1], dtype=torch.float))
+            
+            self.view1.append(self.activation)
+            self.view2.append(self.activation)
+            
+        self.view1.append(nn.Dropout(self.dropout))
+        self.view2.append(nn.Dropout(self.dropout))
+        
+        self.view1.append(
+            nn.Linear(self.layers_before_concat[-2], self.layers_before_concat[-1], dtype=torch.float)
+            )
+        self.view2.append(
+            nn.Linear(self.layers_before_concat[-2], self.layers_before_concat[-1], dtype=torch.float)
+        )
+        
+        self.view1 = nn.Sequential(*self.view1)
+        self.view2 = nn.Sequential(*self.view2)
+        
+        for i in range(len(self.layers_before_concat)-2):
+            
+            self.postconcat.append(
+                nn.Linear(self.layers_after_concat[i], self.layers_after_concat[i+1], dtype=torch.float)
+            )
+            self.postconcat.append(
+                nn.BatchNorm1d(self.layers_after_concat[i+1], dtype=torch.float)
+            )
+            self.postconcat.append(self.activation)
+        
+        self.postconcat.append(
+            nn.Linear(self.layers_after_concat[-2], self.layers_after_concat[-1], dtype=torch.float)
+        )
+        self.postconcat.append(nn.Dropout(self.dropout))
+        self.postconcat.append(
+            nn.Linear(self.layers_after_concat[-1], self.output_size, dtype=torch.float)
+        )
+        self.postconcat = nn.Sequential(*self.postconcat)
+        
+    def forward(self, x):
+        
+        x1 = self.view1(x[:, :self.input_size[0]])
+        x2 = self.view2(x[:, self.input_size[0]:])
+        
+        x = torch.cat((x1, x2), dim=1)
+        x = self.postconcat(x)
+        
+        return x
+
 if __name__ == "__main__":
     
     # Testing
-    model = Net(
-        hidden_layers=[512, 256, 128],
-        input_size=6000,
+    # model = Net(
+    #     hidden_layers=[512, 256, 128],
+    #     input_size=6000,
+    #     output_size=1,
+    #     dropout=0.2,
+    #     activation= "celu"
+    # )
+    
+    model = MultiViewNet(
+        layers_before_concat=[512, 256, 128],
+        layers_after_concat=[512, 256, 128],
+        input_size=[600,600],
         output_size=1,
         dropout=0.2,
-        activation= "celu"
     )
     
-    test = torch.randn(10, 6000, dtype=torch.float32)
+    test = torch.randn(1000, 1200, dtype=torch.float32)
     
     print(model(test))
